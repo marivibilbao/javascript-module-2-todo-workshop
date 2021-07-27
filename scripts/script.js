ec1182ff02dbfed261428800a23280f4c34b5f9f
curso <curso@LT2BA22462.csis.dmn>
2021-07-27 19:10:10 +0200

    Se agrega ejercicio 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-alert("hello!");
+let mySelector = document.querySelector(".button");
 
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+console.log(mySelector);
+
+//alert("hello!");
+
+//document.querySelector('#new-todo').addEventListener('submit', (e) => {
+//    e.preventDefault();
+//    const text = e.target.elements.text.value.trim();
+//    alert(text);
+//})
