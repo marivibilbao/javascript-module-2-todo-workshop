686d3d4fae2dd6c0afb038a4c3e776bfba609687
curso <curso@LT2BA22462.csis.dmn>
2021-07-27 20:46:17 +0200

    Agrego ejercicio 5 realizado en clase

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,38 @@
 //mySelector.style.backgroundColor = "green";
 //console.log(mySelector);
 
-//Ejercicio 4:
-document.querySelector("#new-todo").addEventListener("submit", (alertSomething));
-
-function alertSomething(evento) {
-  evento.preventDefault();
-  let text = evento.target.elements.text.value.trim();
-  console.log(text);
+//---------------------------------------------------------------------------------------
+//.addEventListener Permite crear eventos
+//Ejercicio 5:
+let todos = [ ];
+function createTodo (text) {
+    todos.push(text);
 }
+
+document.querySelector('#new-todo').addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    const text = evento.target.elements.text.value.trim()
+
+    if (text.length > 0) {
+        createTodo(text)
+        evento.target.elements.text.value = ''
+    }
+
+    console.log(todos)
+})
+
+//---------------------------------------------------------------------------------------
+
+//Ejercicio 4:
+//document.querySelector("#new-todo").addEventListener("submit", (alertSomething));
+
+//function alertSomething(evento) {
+//  evento.preventDefault();
+//  let text = evento.target.elements.text.value.trim();
+//  console.log(text);
+//}
+
+//---------------------------------------------------------------------------------------
 
 //Otra manera de hacer el ejercicio 4:
 //document.querySelector('#new-todo').addEventListener('submit', (e) => {
