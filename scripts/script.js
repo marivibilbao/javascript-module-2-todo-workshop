a6abaf031cf2f5c8ed3a45178e9c88442294b2f4
curso <curso@LT2BA22462.csis.dmn>
2021-07-27 20:03:54 +0200

    Agrego ejercicio 4 en clase

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
-let mySelector = document.querySelector(".button");
-mySelector.style.backgroundColor = "green";
-console.log(mySelector);
+//Ejercicios 2 y 3:
+//let mySelector = document.querySelector(".button");
+//mySelector.style.backgroundColor = "green";
+//console.log(mySelector);
 
+document.querySelector("#new-todo").addEventListener("submit", (alertSomething));
+
+function alertSomething(evento) {
+  evento.preventDefault();
+  let text = evento.target.elements.text.value.trim();
+  console.log(text);
+}
+
+//Contenido del archivo inicial:
 //alert("hello!");
 
 //document.querySelector('#new-todo').addEventListener('submit', (e) => {
