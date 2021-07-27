0680fd4b7d11b034bfa470eb0644fe814fdd04ea
curso <curso@LT2BA22462.csis.dmn>
2021-07-27 21:31:33 +0200

    Agrego ejercicio 6 en clase

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,42 @@
-//Ejercicios 2 y 3:
-//let mySelector = document.querySelector(".button");
-//mySelector.style.backgroundColor = "green";
-//console.log(mySelector);
+//Ejercicio 6:
+const generateTodoDOM = (todo) => { //1. Crear función que toma un parámetro: todo.
+    const todoEl = document.createElement('label'); //2. Se crean tres elementos que sólo estan en el JavaScript
+    const containerEl = document.createElement('div'); //3.
+    const todoText = document.createElement('span'); //4.
+    
+    todoText.textContent = todo; //5. 
+    
+    containerEl.appendChild(todoText); //6.
+    
+    todoEl.classList.add('list-item__container'); //7.
+    containerEl.classList.add('list-item__container'); //7.
+    
+    todoEl.appendChild(containerEl); //8.
 
-//---------------------------------------------------------------------------------------
-//.addEventListener Permite crear eventos
-//Ejercicio 5:
-let todos = [ ];
-function createTodo (text) {
-    todos.push(text);
+    return todoEl; //9. Retornar 
 }
 
-document.querySelector('#new-todo').addEventListener('submit', (evento) => {
-    evento.preventDefault()
-    const text = evento.target.elements.text.value.trim()
 
-    if (text.length > 0) {
-        createTodo(text)
-        evento.target.elements.text.value = ''
-    }
+//---------------------------------------------------------------------------------------
 
-    console.log(todos)
-})
+//.addEventListener Permite crear eventos
+//Ejercicio 5:
+//let todos = [ ];
+//function createTodo (text) {
+//.    todos.push(text);
+//}
+
+//document.querySelector('#new-todo').addEventListener('submit', (evento) => {
+//    evento.preventDefault()
+//    const text = evento.target.elements.text.value.trim()
+
+//    if (text.length > 0) {
+//        createTodo(text)
+//        evento.target.elements.text.value = ''
+//    }
+
+//    console.log(todos)
+//})
 
 //---------------------------------------------------------------------------------------
 
@@ -43,7 +58,14 @@ document.querySelector('#new-todo').addEventListener('submit', (evento) => {
 //    console.log(text)
 //})
 
+//---------------------------------------------------------------------------------------
 
+//Ejercicios 2 y 3:
+//let mySelector = document.querySelector(".button");
+//mySelector.style.backgroundColor = "green";
+//console.log(mySelector);
+
+//---------------------------------------------------------------------------------------
 
 //Contenido del archivo inicial:
 //alert("hello!");
